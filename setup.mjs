deb585ab21a681f2c6623dcae34c69e8bc8187ef
Mitch Aplin <mitchaplin@gmail.com>
2023-02-02 17:19:32 -0600

    Complete base functionality

--- setup.mjs
+++ setup.mjs
@@ -1,3 +1,17 @@
-export function setup() {
-  console.log("Hello From My Mod!");
+export function setup(ctx) {
+  ctx.patch(CombatManager, "onEnemyDeath").after(function () {
+    this.player.food.slots.map((x, idx) => {
+      let bankQuantity = this.game.bank.getQty(x.item);
+      if (x.quantity > 0 && bankQuantity > 0) {
+        this.player.equipFood(x.item, bankQuantity);
+        notifyPlayer(
+          this.game.hitpoints,
+          getLangString("TOASTS", "FOOD_EQUIPPED"),
+          "success"
+        );
+      } else {
+        null;
+      }
+    });
+  });
 }
